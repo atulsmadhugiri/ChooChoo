d08b22c939390d65765db54bdfc79233302e99df
Atul Madhugiri <atulm@berkeley.edu>
2024-03-26 18:33:45 -0400

    [ios] Initialize Xcode project

---
+++ Sources/ChooChooApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooChooApp.swift
+//  ChooChoo
+//
+//  Created by atul on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ChooChooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
