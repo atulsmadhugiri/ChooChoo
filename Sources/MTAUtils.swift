4c2bf460f53bce9ed8506520bce5c7c348def06d
Atul Madhugiri <atulm@berkeley.edu>
2024-05-09 19:36:24 -0400

    [ios] Introduce `getColorForLine(line: MTALine) -> Color`

--- Sources/MTAUtils.swift
+++ Sources/MTAUtils.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 enum MTALine {
   case oneTwoThree
@@ -11,4 +12,29 @@ enum MTALine {
   case l
   case nqrw
   case s
+}
+
+func getColorForLine(line: MTALine) -> Color {
+  switch line {
+  case .oneTwoThree:
+    return Color(UIColor(named: "MTAColors/123")!)
+  case .fourFiveSix:
+    return Color(UIColor(named: "MTAColors/456")!)
+  case .seven:
+    return Color(UIColor(named: "MTAColors/7")!)
+  case .ace:
+    return Color(UIColor(named: "MTAColors/ACE")!)
+  case .bdfm:
+    return Color(UIColor(named: "MTAColors/BDFM")!)
+  case .g:
+    return Color(UIColor(named: "MTAColors/G")!)
+  case .jz:
+    return Color(UIColor(named: "MTAColors/JZ")!)
+  case .l:
+    return Color(UIColor(named: "MTAColors/L")!)
+  case .nqrw:
+    return Color(UIColor(named: "MTAColors/NQRW")!)
+  case .s:
+    return Color(UIColor(named: "MTAColors/S")!)
+  }
 }
