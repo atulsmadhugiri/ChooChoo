e7847103152179fb0dc1d3356664faa959ae2631
Atul Madhugiri <atulm@berkeley.edu>
2024-05-09 19:55:11 -0400

    [ios] Introduce `struct MTAStation`

--- Sources/MTAUtils.swift
+++ Sources/MTAUtils.swift
@@ -91,6 +91,32 @@ func getLineForTrain(train: MTATrain) -> MTALine {
 }
 
 func getColorForTrain(train: MTATrain) -> Color {
-    let line = getLineForTrain(train: train)
-    return getColorForLine(line: line)
+  let line = getLineForTrain(train: train)
+  return getColorForLine(line: line)
+}
+
+struct MTAStation {
+  let stationID: Int
+  let complexID: Int
+  let gtfsStopID: String
+  let division: String
+  let line: String
+  let stopName: String
+  let borough: String
+  let daytimeRoutes: String
+  let structure: String
+  let gtfsLatitude: Double
+  let gtfsLongitude: Double
+  let northDirectionLabel: String
+  let southDirectionLabel: String
+  let ada: String
+  let adaDirectionNotes: String
+  let adaNB: String
+  let adaSB: String
+  let capitalOutageNB: String
+  let capitalOutageSB: String
+
+  var location: CLLocation {
+    CLLocation(latitude: self.gtfsLatitude, longitude: self.gtfsLongitude)
+  }
 }
