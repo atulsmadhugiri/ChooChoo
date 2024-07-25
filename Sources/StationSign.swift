804814f7816c47c62a0b885e333fc00c971b37c9
Atul Madhugiri <atulm@berkeley.edu>
2024-07-25 17:14:53 -0400

    [ios] Actually include `StationSign`...

--- Sources/StationSign.swift
+++ Sources/StationSign.swift
@@ -1,18 +1,32 @@
-//
-//  StationSign.swift
-//  ChooChoo
-//
-//  Created by atul on 7/25/24.
-//
-
 import SwiftUI
 
 struct StationSign: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var stationName: String
+  var trains: [MTATrain]
+  var body: some View {
+    HStack {
+      Text(stationName)
+        .font(.title)
+        .foregroundStyle(.white)
+        .bold().padding()
+      Spacer()
+      HStack {
+        ForEach(trains) { route in
+          TrainBadge(train: route, badgeSize: .small)
+        }
+      }.padding()
+    }.background(Color.black)
+      .cornerRadius(4)
+      .overlay(
+        Rectangle()
+          .foregroundColor(.white)
+          .frame(height: 2)
+          .padding(.top, 12),
+        alignment: .top
+      )
+  }
 }
 
 #Preview {
-    StationSign()
+  StationSign(stationName: "Eighth Avenue Station", trains: [.a, .c, .e])
 }
