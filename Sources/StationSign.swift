55dae59107086e68d44bc4ebd1a013aaf272a184
Atul Madhugiri <atulm@berkeley.edu>
2024-07-25 17:13:45 -0400

    [ios] Introduce `StationSign`
    Will need to try this with more stations to make sure it looks right in different scenarios (eg super long station name or station with a lot of routes).

---
+++ Sources/StationSign.swift
@@ -0,0 +1,18 @@
+//
+//  StationSign.swift
+//  ChooChoo
+//
+//  Created by atul on 7/25/24.
+//
+
+import SwiftUI
+
+struct StationSign: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StationSign()
+}
