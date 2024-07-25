21ad3c5b625cfaec82ff78506df6b693eef835cb
Atul Madhugiri <atulm@berkeley.edu>
2024-07-25 15:56:59 -0400

    [ios] Display list of `trainArrivals` in `ContentView`

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct ContentView: View {
   @StateObject private var locationFetcher = LocationFetcher()
+  @State var trainArrivals: [TrainArrivalEntry] = []
+
   var body: some View {
     VStack {
       if let nearestStation = locationFetcher.nearestStation {
@@ -12,12 +14,24 @@ struct ContentView: View {
           Text(nearestStation.stopName)
             .font(.title2)
         }
+        Text("GTFS Stop ID: \(nearestStation.gtfsStopID)")
 
         HStack {
           ForEach(nearestStation.daytimeRoutes) { route in
             TrainBadge(train: route, badgeSize: .small)
           }
         }
+
+        List(trainArrivals) { arrival in
+          if arrival.arrivalTime.timeIntervalSinceNow > 0 {
+            HStack {
+              TrainBadge(train: arrival.train, badgeSize: .small).padding()
+              Text("\(formatTimeInterval(interval: arrival.arrivalTime.timeIntervalSinceNow))")
+                .font(.title3)
+                .monospaced()
+            }
+          }
+        }.listStyle(.plain)
       }
     }
     .padding()
