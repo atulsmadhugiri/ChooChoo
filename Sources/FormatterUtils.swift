2996955784250478367ef3c8dcc43ba529fbc549
Atul Madhugiri <atulm@berkeley.edu>
2024-08-06 17:02:51 -0400

    [ios] Update `formatTimeInterval(interval: TimeInterval)`

--- Sources/FormatterUtils.swift
+++ Sources/FormatterUtils.swift
@@ -2,8 +2,8 @@ import Foundation
 
 func formatTimeInterval(interval: TimeInterval) -> String {
   let formatter = DateComponentsFormatter()
-  formatter.allowedUnits = [.hour, .minute, .second]
-  formatter.unitsStyle = .abbreviated
+  formatter.allowedUnits = [.hour, .minute]
+  formatter.unitsStyle = .short
   let formatted = formatter.string(from: interval) ?? ""
   return formatted
 }
