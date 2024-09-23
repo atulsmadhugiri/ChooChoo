6fc28ebd89d504a9363631e16f8a498d54bac7a4
Atul Madhugiri <atulm@berkeley.edu>
2024-09-23 15:49:14 -0400

    [ios] Configure `PostHog` in `App.init()`
    Just so we can get some basic analytics and can add some logging.

--- Sources/ChooChooApp.swift
+++ Sources/ChooChooApp.swift
@@ -1,7 +1,18 @@
+import PostHog
 import SwiftUI
 
 @main
 struct ChooChooApp: App {
+  init() {
+    let POSTHOG_API_KEY = Secrets.POSTHOG_API_KEY
+    let POSTHOG_HOST = "https://us.i.posthog.com"
+    let configuration = PostHogConfig(
+      apiKey: POSTHOG_API_KEY,
+      host: POSTHOG_HOST
+    )
+    PostHogSDK.shared.setup(configuration)
+  }
+
   var body: some Scene {
     WindowGroup {
       ContentView()
