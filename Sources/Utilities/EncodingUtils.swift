9bb6561bd83fccdb5457fd457962fa48e3659e3e
Atul Madhugiri <atulm@berkeley.edu>
2024-10-02 16:12:06 -0400

    [ios] Cut `swapTripShapeDirection(tripID: String)`
    Glad we get to cut this.

--- Sources/Utilities/EncodingUtils.swift
+++ Sources/Utilities/EncodingUtils.swift
@@ -27,27 +27,6 @@ func tripDirection(for tripID: String) -> TripDirection {
   }
 }
 
-func swapTripShapeDirection(tripID: String) -> String {
-  let components = tripID.components(separatedBy: "..")
-
-  guard components.count == 2 else {
-    return tripID
-  }
-
-  let prefix = components[0]
-  let suffix = components[1]
-
-  let modifiedSuffix: String
-  if suffix.hasPrefix("N") {
-    modifiedSuffix = "S" + suffix.dropFirst()
-  } else if suffix.hasPrefix("S") {
-    modifiedSuffix = "N" + suffix.dropFirst()
-  } else {
-    modifiedSuffix = suffix
-  }
-  return "\(prefix)..\(modifiedSuffix)"
-}
-
 func shapeIDFromTripID(_ tripID: String) -> String {
   guard let underscoreIndex = tripID.firstIndex(of: "_") else { return "" }
   let startIndex = tripID.index(after: underscoreIndex)
