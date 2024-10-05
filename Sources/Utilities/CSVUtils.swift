e51a3175ab0678db893568c275331f69e255e876
Atul Madhugiri <atulm@berkeley.edu>
2024-10-04 21:41:03 -0400

    [Sources] Introduce `tripEntriesFromCSV`
    Gives us an array of (persistable) `TripEntry`s from CSV. This process seems to take 0.15 seconds right now. Maybe we can skip `TabularData` and do this manually faster? We could also drop unused columns, have GTFS stopID instead of tripHeadSign, etc. For now let's say this is acceptable...

--- Sources/Utilities/CSVUtils.swift
+++ Sources/Utilities/CSVUtils.swift
@@ -87,3 +87,31 @@ func shapeToTerminusFromCSV() -> [String: String] {
   }
   return shapeToTerminus
 }
+
+func tripEntriesFromCSV() -> [TripEntry] {
+  var tripEntries: [TripEntry] = []
+  guard
+    let tripsFile = Bundle.main.url(forResource: "Trips", withExtension: "csv")
+  else {
+    print("Trips.csv not found.")
+    return tripEntries
+  }
+
+  do {
+    let df = try DataFrame(contentsOfCSVFile: tripsFile)
+    for row in df.rows {
+      let tripEntry = TripEntry(
+        tripID: row["trip_id"] as! String,
+        routeID: row["route_id"] as! String,
+        tripHeadSign: row["trip_headsign"] as! String,
+        directionID: row["direction_id"] as! Int
+      )
+      tripEntries.append(tripEntry)
+    }
+
+  } catch {
+    print("Error reading CSV file: \(error)")
+  }
+
+  return tripEntries
+}
