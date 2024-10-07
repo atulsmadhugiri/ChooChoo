fdba0fb33ae53e2be73f0df80f9d75a33c749c54
Atul Madhugiri <atulm@berkeley.edu>
2024-10-07 17:55:42 -0400

    [Sources] Add `convenience init?(from row: DataFrame.Row)` to `StopEntry`

--- Sources/Models/StopEntry.swift
+++ Sources/Models/StopEntry.swift
@@ -1,4 +1,5 @@
 import SwiftData
+import TabularData
 
 // Each `MTAStopEntry` corresponds to a GTFS stop in `Stations.csv`
 
@@ -37,5 +38,35 @@ class StopEntry {
     self.gtfsLongitude = gtfsLongitude
     self.northDirectionLabel = northDirectionLabel
     self.southDirectionLabel = southDirectionLabel
+  }
+}
+
+extension StopEntry {
+  convenience init?(from row: DataFrame.Row) {
+    guard
+      let complexID = row["Complex ID"] as? Int,
+      let gtfsStopID = row["GTFS Stop ID"] as? String,
+      let division = row["Division"] as? String,
+      let line = row["Line"] as? String,
+      let stopName = row["Stop Name"] as? String,
+      let daytimeRoutesString = row["Daytime Routes"] as? String,
+      let gtfsLatitude = row["GTFS Latitude"] as? Double,
+      let gtfsLongitude = row["GTFS Longitude"] as? Double
+    else {
+      return nil
+    }
+
+    self.init(
+      gtfsStopID: gtfsStopID,
+      complexID: complexID,
+      division: division,
+      line: line,
+      stopName: stopName,
+      daytimeRoutesString: daytimeRoutesString,
+      gtfsLatitude: gtfsLatitude,
+      gtfsLongitude: gtfsLongitude,
+      northDirectionLabel: row["North Direction Label"] as? String ?? "",
+      southDirectionLabel: row["South Direction Label"] as? String ?? ""
+    )
   }
 }
