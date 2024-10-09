6413e878a29f202206a5246d21dcbccf00f8a50b
Atul Madhugiri <atulm@berkeley.edu>
2024-10-09 13:51:49 -0400

    [Sources] Get rid of `nearestStation` in `LocationFetcher`
    Can prevent sorting stations based on coordinates twice. Will push all of this into `ContentView`

--- Sources/LocationFetcher.swift
+++ Sources/LocationFetcher.swift
@@ -10,7 +10,6 @@ class LocationFetcher: NSObject, ObservableObject, CLLocationManagerDelegate {
   let locationManager = CLLocationManager()
 
   @Published var location: CLLocation?
-  @Published var nearestStation: MTAStation?
 
   private let distanceThreshold: CLLocationDistance = 15
 
@@ -29,14 +28,6 @@ class LocationFetcher: NSObject, ObservableObject, CLLocationManagerDelegate {
   ) {
     guard locations.last != nil else { return }
     location = locations.last
-    nearestStation = getNearestStation(from: locations.last)
   }
 
-  func getNearestStation(from currentLocation: CLLocation?) -> MTAStation? {
-    guard let currentLocation = currentLocation else { return nil }
-    return mtaStations.min(by: {
-      currentLocation.distance(from: $0.location)
-        < currentLocation.distance(from: $1.location)
-    })
-  }
 }
