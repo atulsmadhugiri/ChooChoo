25a6474668636c0cead198694fcaf0d19079404d
Atul Madhugiri <atulm@berkeley.edu>
2024-10-09 13:25:57 -0400

    [Sources] Introduce `filteredStationEntries` and `sortedStationEntries` to `StationSelectionSheet`
    There's going to be a big commit where we need to make a lot of changes at once, so pulling out these changes.
    
    Going to get things working in "parallel" then swap out `MTAStation` entirely.

--- Sources/Views/StationSelectionSheet.swift
+++ Sources/Views/StationSelectionSheet.swift
@@ -1,7 +1,10 @@
 import CoreLocation
+import SwiftData
 import SwiftUI
 
 struct StationSelectionSheet: View {
+  @Query var stations: [StationEntry]
+
   var location: CLLocation?
   @State private var searchTerm = ""
   @Binding var isPresented: Bool
@@ -17,9 +20,20 @@ struct StationSelectionSheet: View {
     }
   }
 
+  var filteredStationEntries: [StationEntry] {
+    guard !searchTerm.isEmpty else { return stations }
+    return stations.filter {
+      $0.name.localizedCaseInsensitiveContains(searchTerm)
+    }
+  }
+
   var body: some View {
     if let location {
       let sorted = filteredStations.sorted(by: {
+        location.distance(from: $0.location)
+          < location.distance(from: $1.location)
+      })
+      let sortedStationEntries = filteredStationEntries.sorted(by: {
         location.distance(from: $0.location)
           < location.distance(from: $1.location)
       })
