ed2fe733806eb6f06ec52fa69085695c2c1bfbc4
Atul Madhugiri <atulm@berkeley.edu>
2024-10-13 21:09:27 -0400

    [Sources] Add `MTAStopValue` initializer and `getLabelFor(TripDirection)`

--- Sources/Entities/MTAStopValue.swift
+++ Sources/Entities/MTAStopValue.swift
@@ -18,3 +18,34 @@ struct MTAStopValue {
     }
   }
 }
+
+extension MTAStopValue {
+  init(mtaStop: MTAStop) {
+    self.init(
+      gtfsStopID: mtaStop.gtfsStopID,
+      complexID: mtaStop.complexID,
+      division: mtaStop.division,
+      line: mtaStop.line,
+      stopName: mtaStop.stopName,
+      daytimeRoutesString: mtaStop.daytimeRoutesString,
+      gtfsLatitude: mtaStop.gtfsLatitude,
+      gtfsLongitude: mtaStop.gtfsLongitude,
+      northDirectionLabel: mtaStop.northDirectionLabel,
+      southDirectionLabel: mtaStop.southDirectionLabel
+    )
+  }
+}
+
+extension MTAStopValue {
+  func getLabelFor(direction: TripDirection) -> String {
+    // HACK: Accounting for weird direction labels for 34 St-Hudson Yards.
+    //       As with all the other hacks, there's definitely a better way.
+    let adjustedDirection =
+      self.gtfsStopID == "726" ? direction.flipped : direction
+    if adjustedDirection == .north {
+      return self.northDirectionLabel
+    } else {
+      return self.southDirectionLabel
+    }
+  }
+}
