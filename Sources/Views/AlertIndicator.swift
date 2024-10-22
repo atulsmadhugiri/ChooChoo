93c6d926e1097e672216120018e82235d466c309
Atul Madhugiri <atulm@berkeley.edu>
2024-10-22 02:17:53 -0400

    [Sources] Add tap haptic to `AlertIndicator`

--- Sources/Views/AlertIndicator.swift
+++ Sources/Views/AlertIndicator.swift
@@ -3,8 +3,12 @@ import SwiftUI
 struct AlertIndicator: View {
   let alertCount: Int
   @Binding var alertSheetActive: Bool
+
+  let tapHaptic = UIImpactFeedbackGenerator(style: .heavy)
+
   var body: some View {
     Button {
+      tapHaptic.impactOccurred()
       alertSheetActive = true
     } label: {
       Image(systemName: "exclamationmark.triangle.fill")
@@ -20,6 +24,9 @@ struct AlertIndicator: View {
     }.buttonStyle(.bordered)
       .tint(.yellow)
       .padding(.vertical, 6)
+      .onAppear {
+        tapHaptic.prepare()
+      }
   }
 }
 
