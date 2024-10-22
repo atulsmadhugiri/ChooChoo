3ac8425a3f8b1ea5ff12b66bdcfaf6321978bb25
Atul Madhugiri <atulm@berkeley.edu>
2024-10-22 01:09:29 -0400

    [Sources] Introduce `parseAlertBody(...)` to render `TrainBadge`s in service alert text
    Surprised this work. Basically worked backwords from the manual concatenation approach that I got working with the "Delays on the [F] and [G] trains example. Thank god we don't need to use `HStack` or whatever and deal with all the text layout weirdness.
    
    I'm not sure if this is the most performant approach, but it works for now so will leave it.

--- Sources/Views/AlertBox.swift
+++ Sources/Views/AlertBox.swift
@@ -5,17 +5,59 @@ struct AlertBox: View {
   var body: some View {
     GroupBox {
       VStack(alignment: .leading) {
-        Text(alertBody)
+        parseAlertBody(alertBody)
+          .fontDesign(.rounded)
       }
     } label: {
-      Label("Alert", systemImage: "exclamationmark.triangle.fill")
+      Label("Service Alert", systemImage: "exclamationmark.triangle.fill")
         .foregroundStyle(.orange)
+        .fontDesign(.rounded)
     }
     .backgroundStyle(.yellow.opacity(0.1))
     .overlay(
       RoundedRectangle(cornerRadius: 8)
         .stroke(Color.yellow, lineWidth: 1)
     )
+  }
+
+  func parseAlertBody(_ alertBody: String) -> Text {
+    let updatedAlertBody = alertBody.replacingOccurrences(
+      of: "\n", with: ".\n\n")
+
+    var result = Text("")
+    var currentIndex = updatedAlertBody.startIndex
+
+    let pattern = /\[([A-Za-z]+)\]/
+
+    while currentIndex < updatedAlertBody.endIndex {
+      if let match = updatedAlertBody[currentIndex...].firstMatch(of: pattern) {
+        let range = match.range
+        let lineSymbol = match.output.1
+
+        let prefixText = String(
+          updatedAlertBody[currentIndex..<range.lowerBound])
+        result = result + Text(prefixText)
+
+        let imageName = "\(lineSymbol.lowercased()).circle.fill"
+        let lineColor =
+          MTATrain(rawValue: String(lineSymbol))?.color ?? MTATrain.a.color
+        let lineText = Text(
+          Image(systemName: imageName)
+            .renderingMode(.original)
+        )
+        .foregroundStyle(lineColor)
+
+        result = result + lineText
+
+        currentIndex = range.upperBound
+      } else {
+        let suffixText = String("\(updatedAlertBody[currentIndex...]).")
+        result = result + Text(suffixText)
+        break
+      }
+    }
+
+    return result
   }
 }
 
