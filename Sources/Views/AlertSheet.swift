ede3b5fec348da5fad248e6154797c24b5b518b4
Atul Madhugiri <atulm@berkeley.edu>
2025-02-01 20:39:07 -0500

    [Sources] Sort service alerts based on beginning of `activePeriods`

--- Sources/Views/AlertSheet.swift
+++ Sources/Views/AlertSheet.swift
@@ -3,27 +3,31 @@ import SwiftUI
 struct AlertSheet: View {
   let serviceAlerts: [MTAServiceAlert]
 
+  var sortedAlerts: [MTAServiceAlert] {
+    serviceAlerts.sorted { lhs, rhs in
+      if lhs.earliestStart != rhs.earliestStart {
+        return lhs.earliestStart < rhs.earliestStart
+      } else {
+        return lhs.earliestEnd < rhs.earliestEnd
+      }
+    }
+  }
+
   var body: some View {
     NavigationView {
       List {
-        ForEach(serviceAlerts) { serviceAlert in
-          AlertBox(alertBody: serviceAlert.header)
+        ForEach(sortedAlerts) { alert in
+          AlertBox(alertBody: alert.header, activePeriods: alert.activePeriod)
             .listRowSeparator(.hidden)
             .backgroundStyle(.thickMaterial)
             .scrollContentBackground(.hidden)
             .listRowBackground(Color.clear)
-
         }
-      }.scrollContentBackground(.hidden)
-        .listStyle(.plain)
-        .navigationBarTitle(
-          "Service Alerts",
-          displayMode: .inline
-        ).backgroundStyle(.thickMaterial)
+      }
+      .scrollContentBackground(.hidden)
+      .listStyle(.plain)
+      .navigationBarTitle("Service Alerts", displayMode: .inline)
+      .backgroundStyle(.thickMaterial)
     }
   }
-}
-
-#Preview {
-  AlertSheet(serviceAlerts: [])
 }
