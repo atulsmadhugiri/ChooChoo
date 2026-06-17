f2367faa2d9de7e07ddc186348f01f1f404a2a66
Atul Madhugiri <atulm@berkeley.edu>
2026-06-17 15:51:41 -0400

    [Entities] Separate route IDs from train badges

--- Sources/Entities/MTATrain.swift
+++ Sources/Entities/MTATrain.swift
@@ -1,7 +1,6 @@
 import Foundation
-import SwiftUI
 
-enum MTATrain: String, CaseIterable, Identifiable {
+public enum MTATrain: String, CaseIterable, Identifiable, Sendable {
   case one = "1"
   case two = "2"
   case three = "3"
@@ -26,11 +25,115 @@ enum MTATrain: String, CaseIterable, Identifiable {
   case w = "W"
   case s = "S"
 
-  var id: String { self.rawValue }
+  public var id: String { self.rawValue }
+}
+
+public enum MTARouteID: String, Sendable {
+  case one = "1"
+  case two = "2"
+  case three = "3"
+  case four = "4"
+  case five = "5"
+  case six = "6"
+  case sixExpress = "6X"
+  case seven = "7"
+  case sevenExpress = "7X"
+  case a = "A"
+  case c = "C"
+  case e = "E"
+  case b = "B"
+  case d = "D"
+  case f = "F"
+  case m = "M"
+  case g = "G"
+  case j = "J"
+  case z = "Z"
+  case l = "L"
+  case n = "N"
+  case q = "Q"
+  case r = "R"
+  case w = "W"
+  case shuttle = "S"
+  case franklinShuttle = "FS"
+  case grandCentralShuttle = "GS"
+  case rockawayShuttle = "H"
+
+  var train: MTATrain {
+    switch self {
+    case .one:
+      return .one
+    case .two:
+      return .two
+    case .three:
+      return .three
+    case .four:
+      return .four
+    case .five:
+      return .five
+    case .six, .sixExpress:
+      return .six
+    case .seven, .sevenExpress:
+      return .seven
+    case .a:
+      return .a
+    case .c:
+      return .c
+    case .e:
+      return .e
+    case .b:
+      return .b
+    case .d:
+      return .d
+    case .f:
+      return .f
+    case .m:
+      return .m
+    case .g:
+      return .g
+    case .j:
+      return .j
+    case .z:
+      return .z
+    case .l:
+      return .l
+    case .n:
+      return .n
+    case .q:
+      return .q
+    case .r:
+      return .r
+    case .w:
+      return .w
+    case .shuttle, .franklinShuttle, .grandCentralShuttle, .rockawayShuttle:
+      return .s
+    }
+  }
+
+  func terminalStopID(for direction: TripDirection) -> String? {
+    switch self {
+    case .grandCentralShuttle:
+      return direction == .north ? "902" : "901"
+    case .rockawayShuttle:
+      return direction == .north ? "H04" : "H15"
+    case .franklinShuttle:
+      return direction == .north ? "S01" : "D26"
+    default:
+      return nil
+    }
+  }
 }
 
 extension MTATrain {
-  var line: MTALine {
+  public init?(routeID: String) {
+    guard let routeID = MTARouteID(rawValue: routeID) else {
+      return nil
+    }
+    self = routeID.train
+  }
+}
+
+extension MTATrain {
+  public var line: MTALine {
     switch self {
     case .a, .c, .e:
       return .ace
@@ -53,11 +156,5 @@ extension MTATrain {
     case .s:
       return MTALine.s
     }
-  }
-}
-
-extension MTATrain {
-  var color: Color {
-    return self.line.color
   }
 }
