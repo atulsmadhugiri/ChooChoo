5a190007d32293a0a358123f87390bd3beb90e2c
Atul Madhugiri <atulm@berkeley.edu>
2026-06-24 20:48:42 -0400

    [Views] Fix MapsButton build integration

--- Sources/Views/StationSign.swift
+++ Sources/Views/StationSign.swift
@@ -1,15 +1,14 @@
 import CoreLocation
-import MapKit
 import SwiftUI
 
 struct StationSign: View {
   var station: MTAStation
-  
+
   let distance: CLLocationDistance?
   let serviceAlerts: [MTAServiceAlert]
-  
+
   @State private var alertSheetActive = false
-  
+
   var body: some View {
     VStack(spacing: 0) {
       VStack(spacing: 0) {
@@ -18,18 +17,43 @@ struct StationSign: View {
             .font(.title)
             .foregroundStyle(.white)
             .bold()
-            .padding(EdgeInsets(top: 18, leading: 12, bottom: 8, trailing: 4))
+            .padding(
+              EdgeInsets(
+                top: 18,
+                leading: 12,
+                bottom: 8,
+                trailing: 4
+              )
+            )
           Spacer()
         }
         HStack {
           ScrollView(.horizontal) {
-            ForEach(station.daytimeRoutes) { route in
-              TrainBadge(train: route)
+            HStack {
+              ForEach(station.daytimeRoutes) { route in
+                TrainBadge(train: route)
+              }
+              Spacer()
             }
-            Spacer()
-          }.padding(EdgeInsets(top: 0, leading: 12, bottom: 12, trailing: 12))
-        }.scrollIndicators(.hidden).background(.black).overlay(Rectangle().fill(.white).frame(height: 2), alignment: .top)
+          }
+        }.padding(
+          EdgeInsets(
+            top: 0,
+            leading: 12,
+            bottom: 12,
+            trailing: 12
+          )
+        )
+        .scrollIndicators(.hidden)
       }
+      .background(.black)
+      .overlay(
+        Rectangle()
+          .fill(.white)
+          .frame(height: 2)
+          .padding(.top, 14),
+        alignment: .top
+      )
 
       HStack {
         Text(distanceText)
@@ -37,25 +61,49 @@ struct StationSign: View {
           .bold()
           .fontDesign(.rounded)
           .foregroundStyle(.secondary)
-          .padding(15, horizontal: 12, vertical: 7)
-          .background(Capsule().fill(Color.secondary.opacity(0.12)))
-          .overlay(Capsule().stroke(Color.secondary.opacity(0.2), lineWidth: 1))
+          .padding(.horizontal, 12)
+          .padding(.vertical, 7)
+          .background(
+            Capsule()
+              .fill(Color.secondary.opacity(0.12))
+          )
+          .overlay(
+            Capsule()
+              .stroke(Color.secondary.opacity(0.2), lineWidth: 1)
+          )
+          .padding(.vertical, 10)
           .accessibilityLabel(distanceText)
 
         Spacer()
 
         if !serviceAlerts.isEmpty {
-          AlertIndicator(alertCount: serviceAlerts.count, alertSheetActive: $alertSheetActive)
+          AlertIndicator(
+            alertCount: serviceAlerts.count,
+            alertSheetActive: $alertSheetActive
+          )
         }
-        
+
         MapsButton(station: station)
         PinButton(station: station)
-      }.padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12)).background(Color(.secondarySystemGroupedBackground))
+      }.padding(
+        EdgeInsets(
+          top: 0,
+          leading: 12,
+          bottom: 0,
+          trailing: 12
+        )
+      ).background(
+        Color(.secondarySystemGroupedBackground)
+      )
 
     }.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
       .sheet(isPresented: $alertSheetActive) {
-        AlertSheet(serviceAlerts: serviceAlerts).presentationDetents([.medium, .large])
-          .presentationDragIndicator(.visible).presentationBackground(.thickMaterial)
+        AlertSheet(serviceAlerts: serviceAlerts)
+          .presentationDetents([
+            .medium, .large,
+          ])
+          .presentationDragIndicator(.visible)
+          .presentationBackground(.thickMaterial)
       }
   }
 
@@ -63,4 +111,3 @@ struct StationSign: View {
     distance.map(formattedDistanceTraveled(distance:)) ?? "Station"
   }
 }
-
